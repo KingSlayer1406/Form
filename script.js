27b0b3e227316d72a55f34b6b15faf49a0d4e86b
Crooked1406 <99968609+KingSlayer1406@users.noreply.github.com>
2023-04-06 03:47:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,48 @@
-function saveData() {
-    // Get form data
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    
-    // Get existing data from local storage
-    var data = JSON.parse(localStorage.getItem("formData")) || [];
-    
-    // Add new data to the array
-    data.push({
-      name: name,
-      email: email,
-      phone: phone
-    });
-    
-    // Save updated data to local storage
-    localStorage.setItem("formData", JSON.stringify(data));
-    
-    // Alert user that data has been saved
-    alert("Data saved successfully!");
-  }
- //var data = JSON.parse(localStorage.getItem("formData")) || [];
+// Get form element and add submit event listener
+const form = document.getElementById("form");
+form.addEventListener("submit", saveData);
 
-//   for (var i = 0; i < data.length; i++) {
-//     var entry = data[i];
-//     console.log("Name: " + entry.name);
-//     console.log("Email: " + entry.email);
-//     console.log("Phone: " + entry.phone);
-//   }
-function searchData() {
-    // Get search term
-    var searchTerm = document.getElementById("searchTerm").value.toLowerCase();
-    
-    // Get data from local storage
-    var data = JSON.parse(localStorage.getItem("formData")) || [];
-    
-    // Filter data based on search term
-    var filteredData = data.filter(function(entry) {
-      return entry.name.toLowerCase().indexOf(searchTerm) !== -1 ||
-             entry.email.toLowerCase().indexOf(searchTerm) !== -1 ||
-             entry.phone.indexOf(searchTerm) !== -1;
-    });
-    
-    // Display filtered data on the page
-    var resultsDiv = document.getElementById("searchResults");
-    resultsDiv.innerHTML = "";
-    
-    if (filteredData.length > 0) {
-      for (var i = 0; i < filteredData.length; i++) {
-        var entry = filteredData[i];
-        resultsDiv.innerHTML += "<p><strong>Name:</strong> " + entry.name + "</p>" +
-                                 "<p><strong>Email:</strong> " + entry.email + "</p>" +
-                                 "<p><strong>Phone:</strong> " + entry.phone + "</p><hr>";
-      }
-    } else {
-      resultsDiv.innerHTML = "<p>No results found.</p>";
-    }
+// Save data to local storage
+function saveData(event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const data = { name, email };
+  let savedData = localStorage.getItem("data");
+  if (savedData === null) {
+    savedData = [];
+  } else {
+    savedData = JSON.parse(savedData);
   }
-  
+  savedData.push(data);
+  localStorage.setItem("data", JSON.stringify(savedData));
+  alert("Data saved successfully!");
+  form.reset();
+}
+
+// Get search button element and add click event listener
+const searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", searchData);
+
+// Search data in local storage and display results
+function searchData() {
+  const searchValue = document.getElementById("search").value;
+  let savedData = localStorage.getItem("data");
+  if (savedData === null) {
+    savedData = [];
+  } else {
+    savedData = JSON.parse(savedData);
+  }
+  const results = savedData.filter((data) => data.name.toLowerCase().includes(searchValue.toLowerCase()));
+  const resultList = document.getElementById("result-list");
+  resultList.innerHTML = "";
+  if (results.length === 0) {
+    resultList.innerHTML = "<li>No results found</li>";
+  } else {
+    results.forEach((data) => {
+      const li = document.createElement("li");
+      li.textContent = `Name: ${data.name}, Email: ${data.email}`;
+      resultList.appendChild(li);
+    });
+  }
+}
